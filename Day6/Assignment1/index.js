8a4598bffcc9dc8c38df1803b4603329635c77d5
Tushar <karansinghrajora1996@gmail.com>
2025-08-03 23:58:49 +0530

    Day 6 Assignment DOM

---
+++ Day6/Assignment1/index.js
@@ -0,0 +1,7 @@
+const hello = document.getElementById("hello");
+const paragraph = document.getElementsByTagName("p");
+const container = document.querySelector(".container");
+
+hello.textContent = "Good Bye From Dominic!";
+paragraph.style.color = "blue";
+container.style.backgroundColor = "yellow";
